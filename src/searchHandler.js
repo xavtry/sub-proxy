462456212482a3e8b20eafb2abb48fec9e978870
xavtry <sebastianishot68@gmail.com>
2025-11-08 20:06:26 -0500

    Update searchHandler.js

--- src/searchHandler.js
+++ src/searchHandler.js
@@ -1,1 +1,5 @@
-
+export function loadSearch(tabId, query) {
+  const iframe = document.getElementById(tabId);
+  const searchURL = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+  iframe.src = `/proxy?url=${encodeURIComponent(searchURL)}`;
+}
