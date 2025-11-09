7a64431254ad5eb91304bd853616e9a2454c4ffa
xavtry <sebastianishot68@gmail.com>
2025-11-08 20:06:53 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const { handleProxy } = require('./proxyHandler');
+const app = express();
+const PORT = 3000;
 
+app.use(express.static('.'));
+
+app.get('/proxy', handleProxy);
+
+app.listen(PORT, () => {
+  console.log(`SUB Recoded V2 running at http://localhost:${PORT}`);
+});
