f94d9f9f16b99f252f7f7fe63989f4430cd941b8
xavtry <sebastianishot68@gmail.com>
2025-11-08 20:08:32 -0500

    Update corsHeaders.js

--- server/corsHeaders.js
+++ server/corsHeaders.js
@@ -1,1 +1,7 @@
+function setCORSHeaders(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+}
 
+module.exports = { setCORSHeaders };
